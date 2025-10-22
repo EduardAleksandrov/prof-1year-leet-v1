6cd1456443635cdb779d68bf7e38e9f83a178afa
Eduard Aleksandrov <eduard.ale@list.ru>
2025-10-22 14:05:17 +0300

    adapter

---
+++ 0.patterns/2.Structural/3.adapter/3.adapter.cpp
@@ -0,0 +1,84 @@
+/*
+    Adapter
+    Адаптер
+*/
+
+// подключаем нужные для работы программы библиотеки
+# include <iostream>
+# include <memory>
+
+// подключаем пространство имен std
+using namespace std;
+
+// класс BaseAdaptee является базовым описанием интерфейса, 
+// который нуждается в адаптации
+ 
+class BaseAdaptee
+{
+public:
+    virtual ~BaseAdaptee() = default;
+    virtual void specificRequest() = 0;
+};
+
+// класс ConcreteAdaptee определяет существующий интерфейс, 
+// который нуждается в адаптации
+// (реализует адаптируемый интерфейс) 
+
+class ConcreteAdaptee : public BaseAdaptee
+{
+public:
+    virtual void specificRequest() override 
+    { 
+        cout << "Method ConcreteAdaptee;" << endl; 
+    }
+};
+
+// Adapter - базовый абстрактный класс для адапетров
+
+// Adapter адаптирует интерфейс Adaptee к зависящему от 
+// предметной области интерфейсу, которым пользуется Client
+
+// Client - сущность, которая вступает во взаимоотношения с объектами, 
+// удовлетворяющими интерфейсу
+ 
+class Adapter
+{
+public:
+    virtual ~Adapter() = default;
+    virtual void request() = 0;
+};
+
+// конкретная реализация экзмпляра BaseAdapter
+
+// ConcreteAdapter адаптирует интерфейс BaseAdaptee к интерфейсу Adapter,
+// чтобы клиентский код мог использовать адаптируемый класс через адаптер
+
+class ConcreteAdapter : public Adapter
+{
+private:
+    shared_ptr<BaseAdaptee>  adaptee;
+public:
+    ConcreteAdapter(shared_ptr<BaseAdaptee> ad) : adaptee(ad) {}
+    void request() override;
+};
+
+void ConcreteAdapter::request()
+{
+    cout << "Adapter: ";
+    if (adaptee)
+    {
+        adaptee->specificRequest();
+    }
+    else
+    {
+        cout << "Empty!" << endl;
+    }
+}
+
+int main()
+{
+    shared_ptr<BaseAdaptee> adaptee = make_shared<ConcreteAdaptee>();
+    shared_ptr<Adapter> adapter = make_shared<ConcreteAdapter>(adaptee);
+
+    adapter->request();
+}
